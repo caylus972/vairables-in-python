4fd3ffd1630e6a57954019ec3f5ae79e7247f1c7
caylus972 <pamigeiadigwe@gmail.com>
2024-12-08 16:49:54 +0000

    secont comit

---
+++ main.py
@@ -0,0 +1,15 @@
+#we are learning about vairables
+x=90
+print(x)
+
+y=56
+print(y)
+
+name="bob"
+print("my name is",name)
+
+name2="orla"
+print("her name is",name2)
+
+name3=input("what is ur name😁")
+print("welcome",name3,"we are happy to have you here🩷")
